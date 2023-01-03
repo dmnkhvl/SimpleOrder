0a8deb53063b898ef1db294bad34a7fe6e56f929
Dominik Havel <dmnkhvl@gmail.com>
2023-01-03 15:16:39 +0100

    refactor: typo

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
       );
     }
     setTotalDiscount(
-      Math.rouund(
+      Math.round(
         totalDiscount + (parseInt(discount) / 100) * 100 * parseInt(amount)
       )
     );
