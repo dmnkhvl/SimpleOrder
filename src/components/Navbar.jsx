d9e2ef3363e6056acdda59161b9cb39543bc76bd
Dominik Havel <dmnkhvl@gmail.com>
2023-01-04 23:07:24 +0100

    style: Navbar change

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div className="w-full h-14 bg-black flex justify-center items-center">
+      <p className="text-white font-medium text-xl">Shopping cart</p>
+    </div>
+  );
+}
+
+export default Navbar;
