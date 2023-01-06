d2ea09e08b5bba657c097d2a9c2e900f3fecaa88
Dominik Havel <dmnkhvl@gmail.com>
2023-01-06 23:43:10 +0100

    style: New font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "sans-serif"],
+        sans: ["Lato", "sans-serif"],
       },
       colors: {
         pastelGreen: "#E3F6FD",
