29eb299dc0161d7cefb8c7d74ff194b4e5fe4152
Dominik Havel <dmnkhvl@gmail.com>
2023-01-07 01:39:30 +0100

    docs: Name change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import { FiShoppingCart } from "react-icons/fi";
 function Header() {
   return (
     <div className="sm:font-semibold sm:text-2xl text-center px-10 py-8 w-full">
-      <p>Order.me</p>
+      <p>SimpleOrder</p>
     </div>
   );
 }
