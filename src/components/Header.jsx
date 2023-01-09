9d3d463097e70a016ca60325c1ef741f8abf3fe8
Dominik Havel <dmnkhvl@gmail.com>
2023-01-09 14:19:55 +0100

    refactor: Name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import { FiShoppingCart } from "react-icons/fi";
 function Header() {
   return (
     <div className="sm:font-semibold sm:text-2xl text-center px-10 py-8 w-full">
-      <p>SimpleOrder</p>
+      <p>SimpleOrder app</p>
     </div>
   );
 }
